d2eca64d8e52a0ceeb80f33a2d90e8e22ed5b970
Washington Silva <w-osilva@hotmail.com>
2017-02-17 11:06:02 -0200

    Alterações em views

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,4 +3,15 @@ class Profile < ApplicationRecord
   extend Enumerize
   enumerize :visibility, in: { public: 0, private: 1 }, predicates: true, scope: true
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def picture=(picture = "/assets/theme/img/avatar.jpg")
+    self[:picture] = picture
+  end
+
+  def picture
+    self[:picture].present? ? self[:picture] : "/assets/theme/img/avatar.jpg"
+  end
 end
